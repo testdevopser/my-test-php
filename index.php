47f2976c5d2954dc258d4fecebbac3312615b791
Serhii <romanko@involve.software>
2025-05-27 14:24:07 +0400

    Первый коммит

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+# testdevopser/my-test-php:latest
+echo "<h3>Kubernetes, hello hello</h3>";
+$server_ip = $_SERVER['SERVER_ADDR'];
+echo "<hr>";
+echo "<p>Server IP: " . $server_ip . "</p>";
+
+?>
